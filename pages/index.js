31d0bdd64d8309c6205580aee23f0e5a7f25c6e8
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-03-22 06:19:18 -0300

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-function Home(){
-    return (
+function Home() {
+  return (
     <div>
-        <h1>Será que vou conseguir construir meu app de fazendas?</h1>
-        <h2>Estou no caminho!</h2>
-    </div>)
-    
+      <h1>Será que vou conseguir construir meu app de fazendas?</h1>
+      <h2>Estou no caminho!</h2>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
