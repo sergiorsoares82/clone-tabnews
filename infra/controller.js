6ca77708991fbd5f429c2972591d62bc436a9585
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-05-15 17:49:49 -0300

    feat: creat and use `NotFoundError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -2,6 +2,7 @@ const {
   MethodNotAllowedError,
   InternalServerError,
   ValidationError,
+  NotFoundError,
 } = require("./errors");
 
 function onNoMatchHandler(req, res) {
@@ -10,7 +11,7 @@ function onNoMatchHandler(req, res) {
 }
 
 function onErrorHandler(err, req, res) {
-  if (err instanceof ValidationError) {
+  if (err instanceof ValidationError || err instanceof NotFoundError) {
     return res.status(err.statusCode).json(err);
   }
 
