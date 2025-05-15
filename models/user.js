96c38abc53c18d41ab2e5346f56b31e9fd49144a
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-05-15 17:51:34 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,35 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
+
+const findOneByUsername = async (username) => {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+      SELECT 
+        * 
+      FROM 
+        users 
+      WHERE 
+        LOWER(username) = LOWER($1)
+      LIMIT 
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username foi digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+};
 
 const create = async (userInputValues) => {
   await validateUniqueEmail(userInputValues.email);
@@ -74,6 +104,7 @@ const create = async (userInputValues) => {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
---
+++ pages/api/v1/users/[username]/index.js
@@ -0,0 +1,14 @@
+import controller from "infra/controller";
+import { createRouter } from "next-connect";
+import user from "models/user.js";
+
+const router = createRouter();
+router.get(getHandler);
+
+export default router.handler(controller.errorHandler);
+
+async function getHandler(req, res) {
+  const { username } = req.query;
+  const foundUser = await user.findOneByUsername(username);
+  return res.status(200).json(foundUser);
+}
